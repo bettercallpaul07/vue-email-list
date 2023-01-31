8189e25a18b2aad3089b8358e3d0c107a3c8e2c1
Paolo Rapuano <bettercallpaul07@gmail.com>
2023-01-31 22:57:18 +0100

    Complete

--- js/scripts.js
+++ js/scripts.js
@@ -11,7 +11,11 @@ createApp ({
 
   data() {
 
+    let init = Date.now();
+    console.log(init);
+
     return {
+      timeesecution: 0,
       mailList: []
 
     };//return
@@ -19,7 +23,7 @@ createApp ({
   },//data
 
   methods: {
-
+    
     // generateMail () {
     //   for (let i = 1; i<=10; i++) {
     //     axios
@@ -35,9 +39,9 @@ createApp ({
 
     },//methods
 
-
-
    mounted() {
+    const self = this;
+    let start = Date.now();
     for (let i = 1; i<=10; i++) {
       axios
         .get("https://flynn.boolean.careers/exercises/api/random/mail")
@@ -47,8 +51,19 @@ createApp ({
             console.log (i, output)
 
             this.mailList.push(output.data.response)
-          });        
-    }}
+          });
+    }
+    let timeTaken = Date.now()
+    console.log(this.init);
+    console.log('------------------------------------');
+    console.log(start);
+    console.log('------------------------------------');
+    console.log(timeTaken);
+
+    this.timeesecution = timeTaken - start;
+    console.log("Total time taken : " + this.timeTaken + " milliseconds");
+  }
+  
 
   
 }).mount("#app")
